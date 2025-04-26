604ee0d7c39cfbbdac67f3d8b635c225982ad05d
Mihai Tarce <mihai.tarce@gmail.com>
2025-04-27 00:21:48 +0800

    Set dataset default path to dataset/.

--- src/main_dataset.py
+++ src/main_dataset.py
@@ -29,7 +29,7 @@ parser.add_argument("--logdir", default="train", type=str, help="directory to sa
 parser.add_argument(
     "--pretrained_dir", default="./pretrained_models/", type=str, help="pretrained checkpoint directory"
 )
-parser.add_argument("--data_dir", default="./dataset_10/", type=str, help="dataset directory")
+parser.add_argument("--data_dir", default="./dataset/", type=str, help="dataset directory")
 parser.add_argument("--json_list", default="dataset_0.json", type=str, help="dataset json file")
 parser.add_argument(
     "--pretrained_model_name",
